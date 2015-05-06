e95d7b13361957eaf46b90c241e21f2be2878506
Maxim Akkuzin <akkmaxon2307@gmail.com>
2015-05-06 17:59:37 +0300

    Version 0.1 ready for using

--- lib/lxf_find.rb
+++ lib/lxf_find.rb
@@ -1,17 +1,33 @@
 module LxfFind
-  def self.find_inside(local_pages,query=nil)
-    puts "What do you search?:"
+  def self.find_inside(local_pages, query = nil)
     unless query
+      puts "What do you search?:"
       query = STDIN::gets.chomp
+    else
+      puts query
     end
     puts "====================\n"
     local_pages.each do |page|
-      filename= LOCAL_DIR + page
+      filename= LOCAL_DIR + page.to_s
       html_source = open(filename).read
       if html_source.include?(query) or html_source.include?(query.upcase) or html_source.include?(query.capitalize)
-        puts "LinuxFormat#" + /\d{2,3}/.match(page)[0] + "\n"
+        puts "LinuxFormat##{page}\n"
       end
     end
     puts "====================\n"
+  end
+  def self.extract_links(url, method, headers = {"User-Agent" => "Ruby #{RUBY_VERSION}"})
+    case method
+    when 'local'
+      archive_page = open(url)
+    when 'online'
+      archive_page = open(url, headers)
+    end
+    page = Nokogiri::HTML(archive_page)
+    links = page.css('div.b-anons-cover a').map do |a|
+      link = a['href']
+      /\d{2,3}/.match(link)[0].to_i
+    end
+    return links
   end
 end
